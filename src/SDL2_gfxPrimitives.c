f51bce5774f098487b11e4197fdaf9887409d057
OJddJO <95376647+OJddJO@users.noreply.github.com>
2024-12-23 01:44:16 +0100

    modification notice in sdl_gfx

--- src/SDL2_gfxPrimitives.c
+++ src/SDL2_gfxPrimitives.c
@@ -2,7 +2,7 @@
 
 SDL2_gfxPrimitives.c: graphics primitives for SDL2 renderers
 
-Copyright (C) 2012-2014  Andreas Schiffler
+Copyright (C) 2012-2014  Andreas Schiffler - modified by OJddJO
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
