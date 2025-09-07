47db6f421082c623857aee28a70e31d02b9bb479
OJddJO <95376647+OJddJO@users.noreply.github.com>
2025-09-07 22:32:29 +0200

    Bump

--- src/SSGE_geometry.c
+++ src/SSGE_geometry.c
@@ -152,7 +152,7 @@ SSGEAPI SSGE_Texture *SSGE_CreateCircle(uint32_t *id, char *name, int x, int y, 
         SDL_SetTextureBlendMode(texture->texture, SDL_BLENDMODE_BLEND);
         SDL_SetRenderTarget(_engine.renderer, texture->texture);
 
-        filledCircleRGBA(_engine.renderer, x, y, radius, color.r, color.g, color.b, color.a);
+        circleRGBA(_engine.renderer, x, y, radius, color.r, color.g, color.b, color.a);
 
         SDL_SetRenderTarget(_engine.renderer, NULL);
         SDL_SetRenderDrawColor(_engine.renderer, _color.r, _color.g, _color.b, _color.a);
@@ -281,6 +281,82 @@ SSGEAPI SSGE_Texture *SSGE_CreateEllipseThick(uint32_t *id, char *name, int x, i
 
         thickEllipseRGBA(_engine.renderer, x, y, rx, ry, color.r, color.g, color.b, color.a, thickness);
 
+        SDL_SetRenderTarget(_engine.renderer, NULL);
+        SDL_SetRenderDrawColor(_engine.renderer, _color.r, _color.g, _color.b, _color.a);
+    }
+
+    _initTextureFields(texture);
+
+    _addToList(&_textureList, texture, name, id, __func__);
+    return texture;
+}
+
+
+SSGEAPI SSGE_Texture *SSGE_CreateFillRect(uint32_t *id, char *name, int x1, int y1, int x2, int y2, SSGE_Color color) {
+    _assert_engine_init
+    SSGE_Texture *texture = (SSGE_Texture *)malloc(sizeof(SSGE_Texture));
+    if (texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture")
+
+    texture->texture = SDL_CreateTexture(_engine.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, _engine.width, _engine.height);
+    if (texture->texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture name")
+    if (color.a != 0) {
+        SDL_SetTextureBlendMode(texture->texture, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderTarget(_engine.renderer, texture->texture);
+
+        boxRGBA(_engine.renderer, x1, y1, x2, y2, color.r, color.g, color.b, color.a);
+
+        SDL_SetRenderTarget(_engine.renderer, NULL);
+        SDL_SetRenderDrawColor(_engine.renderer, _color.r, _color.g, _color.b, _color.a);
+    }
+
+    _initTextureFields(texture);
+
+    _addToList(&_textureList, texture, name, id, __func__);
+    return texture;
+}
+
+SSGEAPI SSGE_Texture *SSGE_CreateFillCircle(uint32_t *id, char *name, int x, int y, int radius, SSGE_Color color) {
+    _assert_engine_init
+    SSGE_Texture *texture = (SSGE_Texture *)malloc(sizeof(SSGE_Texture));
+    if (texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture")
+
+    texture->texture = SDL_CreateTexture(_engine.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, _engine.width, _engine.height);
+    if (texture->texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture name")
+    if (color.a != 0) {
+        SDL_SetTextureBlendMode(texture->texture, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderTarget(_engine.renderer, texture->texture);
+
+        filledCircleRGBA(_engine.renderer, x, y, radius, color.r, color.g, color.b, color.a);
+
+        SDL_SetRenderTarget(_engine.renderer, NULL);
+        SDL_SetRenderDrawColor(_engine.renderer, _color.r, _color.g, _color.b, _color.a);
+    }
+
+    _initTextureFields(texture);
+
+    _addToList(&_textureList, texture, name, id, __func__);
+    return texture;
+}
+
+SSGEAPI SSGE_Texture *SSGE_CreateFillEllipse(uint32_t *id, char *name, int x, int y, int rx, int ry, SSGE_Color color) {
+    _assert_engine_init
+    SSGE_Texture *texture = (SSGE_Texture *)malloc(sizeof(SSGE_Texture));
+    if (texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture")
+
+    texture->texture = SDL_CreateTexture(_engine.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, _engine.width, _engine.height);
+    if (texture->texture == NULL) 
+        SSGE_Error("Failed to allocate memory for texture name")
+    if (color.a != 0) {
+        SDL_SetTextureBlendMode(texture->texture, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderTarget(_engine.renderer, texture->texture);
+    
+        filledEllipseRGBA(_engine.renderer, x, y, rx, ry, color.r, color.g, color.b, color.a);
+    
         SDL_SetRenderTarget(_engine.renderer, NULL);
         SDL_SetRenderDrawColor(_engine.renderer, _color.r, _color.g, _color.b, _color.a);
     }
